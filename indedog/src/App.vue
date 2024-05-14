16959066c0de74df1dcfc4491c17e54363fc8e20
한지웅 <wldnd2977@gmail.com>
2024-05-14 11:59:48 +0900

    nowintheater

--- indedog/src/App.vue
+++ indedog/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <Menu />
     <div style="text-align: center; margin-top: 20px;">
       <RouterLink :to="{name: 'home'}">
         <img src="@/assets/icon/site_icon_1.jpg" class="rounded col" alt="Home" width="100px" height="100px">
       </RouterLink>
     </div>
   <br>
-  <div class="container" style="background-color: aquamarine; width: 1200px;">
+  <div class="container" style="width: 1200px;">
     <div class="row">
         <div>
           <RouterView />
+          <Menu />
         </div>
     </div>
   </div>
---
+++ indedog/src/components/NowInTheater.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class = "container">
+        <div id="carousel_now_in_theater" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="container row align-items-center">
+                    <div class="col-4">
+                        <img src="@/assets/movie_ex1.jpeg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-8">
+                        <h2>이곳에 영화 정보를 적습니다.</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container row align-items-center">
+                    <div class="col-4">
+                        <img src="@/assets/movie_ex2.jpeg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-8">
+                        <h2>이곳에 영화 정보를 적습니다.</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container row align-items-center">
+                    <div class="col-4">
+                        <img src="@/assets/movie_ex3.jpeg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="col-8">
+                        <h2>이곳에 영화 정보를 적습니다.</h2>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carousel_now_in_theater" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel_now_in_theater" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
