31b506b77cf8bb9de1198cd5bb74735b7bd0e041
woongveloper <woongveloper98@gmail.com>
2024-05-18 15:56:51 +0900

    [Front]-reload/split solve

--- indedog/src/components/CinemaDetail-NowInTheater.vue
+++ indedog/src/components/CinemaDetail-NowInTheater.vue
@@ -28,20 +28,31 @@
 <script setup>
 
 import { useCounterStore } from '@/stores/counter'
-import { RouterLink } from 'vue-router'
-import { ref } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
+import { onMounted, ref } from 'vue'
 
+
+const isFirstMount = ref(true)
 const store = useCounterStore()
 const movieList = store.movies
 const recentMovie = ref([])
 const cinemaList = ref([])
 // 키워드를 중심으로 검색
 // 현재 상영중인 영화는 id 10000번대부터 설정, id 1만번대부터 검색
-for (const movie of movieList) {
-    if (Number(movie.id) >= 10000) {
-        recentMovie.value.push(movie)
+
+onMounted(() => {
+    if (isFirstMount.value === true) {
+        for (const movie of movieList) {
+            if (Number(movie.id) >= 10000) {
+                    recentMovie.value.push(movie)
+            }
+        isFirstMount.value = false
     }
 }
+})
+
+
+
 
 
 </script>
--- indedog/src/stores/counter.js
+++ indedog/src/stores/counter.js
@@ -26,6 +26,12 @@ export const useCounterStore = defineStore('counter', () => {
     })
       .then(res => {
         movies.value = res.data
+        for (const idx in movies.value) {
+          if (movies.value[idx].id >= 10000) {
+            movies.value[idx].cinemas = movies.value[idx].cinemas.split(',')
+            console.log(movies.value[idx].cinemas)
+          }
+        }
       })
       .catch(err => console.log(err))
 
@@ -92,6 +98,7 @@ export const useCounterStore = defineStore('counter', () => {
     .then(res => {
       cinemas.value = res.data;
       console.log('#store - cinema')
+      console.log(cinemas.value)
     })
     .catch(err => console.log(err))
   }
