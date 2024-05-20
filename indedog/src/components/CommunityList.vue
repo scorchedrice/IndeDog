ddd22e97043bf8c935d75bedea86cef907cf0327
배원빈 <baebini1997@gmail.com>
2024-05-20 15:21:38 +0900

    [back] community category updated

--- indedog/src/components/CommunityList.vue
+++ indedog/src/components/CommunityList.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
         <h3>게시글 목록</h3>
+        <select name="category" id="category" v-model="category">
+            <option value="영화">영화</option>
+            <option value="상영관">상영관</option>
+            <option value="자유">자유</option>
+            <option value="공지">공지</option>
+        </select>
         <article v-for="article in store.articles"
         :key="article.id">
-            <h3>[{{ article.category }}]</h3>
-            <RouterLink :to="{ name: 'CommunityDetailView', params: { id: article.id }}">
-                <h3>{{ article.title }}</h3>
-            </RouterLink>
+            <div v-if="article.category == category">
+                <h3>[{{ article.category }}]</h3>
+                <RouterLink :to="{ name: 'CommunityDetailView', params: { id: article.id }}">
+                    <h3>{{ article.title }}</h3>
+                </RouterLink>
+            </div>
         </article>
     </div>
 </template>
@@ -14,12 +22,15 @@
 <script setup>
 import { useCounterStore } from '@/stores/counter'
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
 
+const category = ref(null)
 const store = useCounterStore()
 </script>
 
 <style scoped>
 article {
     display: flex;
+    flex-direction: row;
 }
 </style>
